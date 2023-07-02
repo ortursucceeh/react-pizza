2d5c4935efeaadfc36e16d189aca1db403e59490
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-07-02 17:13:23 +0300

    add pizza +-quantity feature

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -9,6 +9,7 @@ function Button({ children, disabled, to, type, onClick }) {
     small: base + ' px-4 py-2 md:px-5 sm:py-2.5 text-xs',
     secondary:
       'inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-transparent border-2 border-stone-400 rounded-full text-stone-400 hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200  focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+    round: base + ' px-2.5 py-1 md:px-3.5 sm:py-2 text-xs',
   };
 
   if (to)
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,15 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   cart: [],
-  // cart: [
-  //   {
-  //     pizzaId: 12,
-  //     name: 'Mediasdasd',
-  //     quantity: 2,
-  //     unitPrice: 16,
-  //     totalPrice: 32,
-  //   },
-  // ],
 };
 
 const cartSlice = createSlice({
@@ -32,6 +23,8 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -57,4 +50,7 @@ export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
-  state.cart.cart.reduce((sum, item) => sum + item.unitPrice, 0);
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
